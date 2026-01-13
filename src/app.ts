a6bd984f7d3d09d162a4dddf59169671430d8423
rayhan2392 <rkshawnso@gmail.com>
2026-01-13 22:23:41 +0600

    project initialized and prisma setup done

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 
+
 const app: Application = express();
 app.use(cors({
     origin: 'http://localhost:3000',
@@ -16,7 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
-        Message: "Ph health care server.."
+        message: "Server is running...",
+        environment: process.env.NODE_ENV,
+        uptime: process.uptime().toFixed(2) + " seconds",
+        timestamp: new Date().toISOString()
     })
 });
 
